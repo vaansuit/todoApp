7bae6ecc6b99acba878808806b69ef82b56b76b4
vaansuit <vvansuit@outlook.com>
2023-01-18 17:22:53 -0300

    update: added first frame

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
-import controller.ProjectController;
-import model.Project;
-
 public class Main {
     public static void main(String[] args) {
 
-        ProjectController projectController = new ProjectController();
-
-        Project project = new Project();
-        project.setProjectName("Projeto Teste");
-        project.setProjectDesc("Esse projeto é um teste");
-        projectController.save(project);
 
     }
 }
---
+++ src/view/MainView.java
@@ -0,0 +1,107 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class MainView {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainView window = new MainView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 949, 886);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel imageHeader = new JLabel("");
+		imageHeader.setIcon(new ImageIcon("C:\\Users\\Usuario\\Documents\\JAVA\\JAVA_APPS\\TodoApp\\src\\img\\background.jpg"));
+		imageHeader.setBounds(0, 0, 933, 240);
+		frame.getContentPane().add(imageHeader);
+		
+		JPanel projectHeaderPanel = new JPanel();
+		projectHeaderPanel.setBackground(Color.WHITE);
+		projectHeaderPanel.setBounds(10, 251, 208, 50);
+		frame.getContentPane().add(projectHeaderPanel);
+		projectHeaderPanel.setLayout(null);
+		
+		JLabel projectHeaderLabel = new JLabel("PROJETOS");
+		projectHeaderLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		projectHeaderLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		projectHeaderLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		projectHeaderLabel.setBounds(10, 11, 119, 28);
+		projectHeaderPanel.add(projectHeaderLabel);
+		
+		JLabel labelProjectAddIcon = new JLabel("");
+		labelProjectAddIcon.setBackground(Color.BLACK);
+		labelProjectAddIcon.setBounds(139, 0, 59, 50);
+		ImageIcon addProjectIcon = new ImageIcon("C:\\\\Users\\\\Usuario\\\\Documents\\\\JAVA\\\\JAVA_APPS\\\\TodoApp\\\\src\\\\img\\\\addIcon.png");
+		Image imageAddProjectIcon = addProjectIcon.getImage();
+		Image addProjectIconScale = imageAddProjectIcon.getScaledInstance(labelProjectAddIcon.getWidth(), labelProjectAddIcon.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon scaleIcon = new ImageIcon(addProjectIconScale);
+		labelProjectAddIcon.setIcon(scaleIcon);
+		projectHeaderPanel.add(labelProjectAddIcon);
+		
+		
+		JPanel taskHeaderPanel = new JPanel();
+		taskHeaderPanel.setBackground(Color.WHITE);
+		taskHeaderPanel.setBounds(228, 251, 695, 50);
+		frame.getContentPane().add(taskHeaderPanel);
+		taskHeaderPanel.setLayout(null);
+		
+		JLabel taskHeaderLabel = new JLabel("TAREFAS");
+		taskHeaderLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		taskHeaderLabel.setBounds(10, 11, 102, 28);
+		taskHeaderPanel.add(taskHeaderLabel);
+		
+		JPanel projectBodyPanel = new JPanel();
+		projectBodyPanel.setBackground(Color.WHITE);
+		projectBodyPanel.setBounds(10, 306, 208, 530);
+		frame.getContentPane().add(projectBodyPanel);
+		projectBodyPanel.setLayout(null);
+		
+		JPanel projectBodyTask = new JPanel();
+		projectBodyTask.setBackground(Color.WHITE);
+		projectBodyTask.setBounds(228, 306, 695, 530);
+		frame.getContentPane().add(projectBodyTask);
+		projectBodyTask.setLayout(null);
+		
+	}
+}
